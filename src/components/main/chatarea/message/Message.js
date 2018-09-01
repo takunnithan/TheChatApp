ac9f05ec63ab92204243c9785457490703eb8be0
takunnithan <takunnithan@gmail.com>
2018-09-01 15:00:04 +0530

    Added fontawesome package

--- src/components/main/chatarea/message/Message.js
+++ src/components/main/chatarea/message/Message.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import classes from './Message.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons'
 
 class Message extends Component {
   render() {
     return (
       <div className={classes.message_container} >
-        <div className={classes.edit_container} ></div>
+        <div className={classes.edit_container} ><div><FontAwesomeIcon icon={faEllipsisH} color='#989898' /></div></div>
         <div className={classes.profile_pic_container}>
         <div className={classes.profile_pic} ><img src={this.props.avatar} alt=''/></div>
         </div>
