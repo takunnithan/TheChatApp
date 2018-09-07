400c2604656f9b10bd22829ab3555b0bfbcd7b7f
takunnithan <takunnithan@gmail.com>
2018-09-08 02:47:17 +0530

    Added message edit dialog box, buttons

--- src/components/main/chatarea/message/Message.js
+++ src/components/main/chatarea/message/Message.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Message.css';
 import Edit from './edit_component/Edit';
+import chat_box_classes from '../../chatbox/ChatBox.css';
 
 class Message extends Component {
 
@@ -27,7 +28,7 @@ class Message extends Component {
       {
         this.state.showEdit
         ? (
-            <div>Worked!</div>
+          <EditDialogBox avatar={this.props.avatar} message={this.props.message} />
         )
         :(
           <div className={classes.message_container} >
@@ -53,3 +54,29 @@ class Message extends Component {
 }
 
 export default Message;
+
+
+class EditDialogBox extends Component {
+  render() {
+    return (
+    <div className={classes.message_container}>
+    <div>
+      <div className={classes.profile_pic_container}>
+        <div className={classes.profile_pic} ><img src={this.props.avatar} alt=''/></div>
+      </div>
+      <div className={classes.chatbox} >
+        <input className={classes.text_area}
+               defaultValue={this.props.message} 
+               type="text"
+               />
+      </div>
+    </div>
+      <div className={classes.edit_button_container}>
+        <button className={classes.cancel_button} >Cancel</button>
+        <button className={classes.save_button} >Save</button>
+      </div>
+
+    </div>
+    );
+  }
+}
