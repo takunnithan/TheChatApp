a42f7d0663447b0074a21659037e452c4e70128c
takunnithan <takunnithan@gmail.com>
2018-09-08 18:56:07 +0530

    Update message Done

--- src/components/main/chatarea/message/Message.js
+++ src/components/main/chatarea/message/Message.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import classes from './Message.css';
 import Edit from './edit_component/Edit';
+import axios from 'axios';
 
 class Message extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     
     this.state = {
       showEdit: false,
-      showDelete: false
+      showDelete: false,
+      message: props.message,
+      message_id: props.id,
+      update_message: props.message
     };
     
     this.editClickHandler = this.editClickHandler.bind(this);
     this.cancelHandler = this.cancelHandler.bind(this);
-    // this.closeMenu = this.closeMenu.bind(this);
+    this.saveHandler = this.saveHandler.bind(this);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
   }
 
   editClickHandler() {
@@ -23,6 +28,23 @@ class Message extends Component {
 
   cancelHandler() {
     this.setState({ showEdit: false });
+  }
+
+  saveHandler() {
+    var payload =     {
+        "created_at": "2018-09-08T20:10:12Z",
+        "message": this.state.update_message
+    }
+    var patch_url = 'http://localhost:8000/messages/' + this.state.message_id +'/';
+
+    // TODO: Get the response and if status is not 200 then - Failure message / Retry option
+    axios.patch(patch_url, payload);
+    this.setState({ message : this.state.update_message });
+    this.setState({ showEdit: false });
+  }
+
+  onChangeHandler(e) {
+    this.setState({ update_message : e.target.value });
   }
 
 
@@ -34,8 +56,10 @@ class Message extends Component {
         ? (
           <EditDialogBox 
             avatar={this.props.avatar} 
-            message={this.props.message}
-            cancelHandler={this.cancelHandler} />
+            message={this.state.message}
+            cancelHandler={this.cancelHandler}
+            saveHandler={this.saveHandler}
+            onChangeHandler={this.onChangeHandler} />
         )
         :(
           <div className={classes.message_container} >
@@ -49,7 +73,7 @@ class Message extends Component {
                 <div className={classes.message_time}>{this.props.time}</div>
               </div>
               <div className={classes.message_body}>
-                {this.props.message}
+                {this.state.message}
               </div>
             </div>
           </div>
@@ -65,7 +89,6 @@ export default Message;
 
 class EditDialogBox extends Component {
 
-
   render() {
     return (
     <div className={classes.message_container}>
@@ -77,12 +100,13 @@ class EditDialogBox extends Component {
         <input className={classes.text_area}
                defaultValue={this.props.message} 
                type="text"
+               onChange={this.props.onChangeHandler}
                />
       </div>
     </div>
       <div className={classes.edit_button_container}>
         <button className={classes.cancel_button} onClick={this.props.cancelHandler}>Cancel</button>
-        <button className={classes.save_button} >Save</button>
+        <button className={classes.save_button} onClick={this.props.saveHandler}>Save</button>
       </div>
 
     </div>
