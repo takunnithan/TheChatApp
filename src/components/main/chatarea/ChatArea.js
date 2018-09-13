6577addaead15cdbf0d067f818088f743e0d951c
takunnithan <takunnithan@gmail.com>
2018-09-13 22:57:04 +0530

    Added Auto Scrolling and minor CSS fixes

--- src/components/main/chatarea/ChatArea.js
+++ src/components/main/chatarea/ChatArea.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import classes from './ChatArea.css';
 import Message from './message/Message';
 import { connect } from 'react-redux';
+import * as ReactDOM from 'react-dom';
 
 class ChatArea extends Component {
+
+  scrollToBottom = () => {
+    const { messageList } = this.refs;
+    const scrollHeight = messageList.scrollHeight;
+    const height = messageList.clientHeight;
+    const maxScrollTop = scrollHeight - height;
+    ReactDOM.findDOMNode(messageList).scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+    }
+
+  componentDidUpdate() {
+      this.scrollToBottom();
+  }
 
   render() {
     const messages = this.props.messages.map(message => {
@@ -16,8 +29,8 @@ class ChatArea extends Component {
                 avatar={message.avatar} />
   })
     return (
-      <div className={classes.chat_area} >
-        <div className={classes.messages} >
+      <div className={classes.chat_area} ref="messageList" >
+        <div className={classes.messages}>
             {messages}
         </div>
       </div>
