7fa7aee2610f9db78a3cbbe316a3f56239436ad0
takunnithan <takunnithan@gmail.com>
2018-09-14 01:36:11 +0530

    Added login page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import classes from './App.css';
 
 import Sidebar from './components/sidebar/Sidebar';
 import Main from './components/main/Main';
+import Login from './components/login/Login';
 
 
 class App extends Component {
   render() {
     return (
-      <div className={classes.app} >
-        <div className={classes.sidebar} ><Sidebar/></div>
-        <div className={classes.main} ><Main /></div>
-      </div>
+      // <div className={classes.app} >
+      //   <div className={classes.sidebar} ><Sidebar/></div>
+      //   <div className={classes.main} ><Main /></div>
+      // </div>
+      <Login />
     );  
   }
 }
---
+++ src/components/login/Login.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import classes from './Login.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className={classes.login_main} >
+      <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"></link>
+        <div className={classes.login_box}>
+        <div className={classes.thechatapp_logo}>TheChatApp</div>
+        <div className={classes.inputs}>
+          <input className={classes.input} defaultValue='username' />
+          <input className={classes.input} defaultValue='password' />
+          </div>
+        <div className={classes.login_button_container}>
+          <button className={classes.login_button}>Login</button>
+        </div>
+        <div className={classes.remember_me}>
+          <input type="checkbox" name="remember"/> Remember me
+        </div>
+      </div>
+      </div>
+    );  
+  }
+}
+
+export default Login;
