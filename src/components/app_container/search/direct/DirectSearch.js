5b1d4b6e99fb13f5ce7d55b39d87451769964127
takunnithan <takunnithan@gmail.com>
2019-01-15 22:35:09 +0530

    Added User search Display search results

--- src/components/app_container/search/direct/DirectSearch.js
+++ src/components/app_container/search/direct/DirectSearch.js
@@ -3,35 +3,56 @@ import SearchTemplate from '../search_template/SearchTemplate';
 import SearchResult from '../search_template/search_result/SearchResult';
 import classes from './DirectSearch.css';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 
 class DirectSearch extends Component{
 
+    state = {
+        results: []
+    }
+
+    userSearch = (event) => {
+        var message = event.target.value;
+        if (message){
+            axios(
+                {
+                    method: 'get',
+                    url: 'http://localhost:8000/search/user',
+                    headers: {
+                        'auth-token':localStorage.getItem('auth_token'),
+                        'user-id': localStorage.getItem('user_id')
+                    },
+                    params: {
+                        'q': message
+                    }
+                }).then(response => {
+                    var data = response.data;
+                    this.setState({results: data});
+            });
+        }
+    }
+
     render() {
+        var users = null;
+        if (this.state.results){
+          users = Object.values(this.state.results).map(user => {
+            return <a key={user.user_id}><SearchResult><h3>{user.user_name}</h3></SearchResult></a>
+        })
+        }
         return (
             <SearchTemplate show={this.props.showDirectChatSearch} type='direct' >
                 <h1>Direct Chat</h1>
                 <div className={classes.search_box_container}>
-                    <div className={classes.search_box_div}><input className={classes.search_box} placeholder='Search for users' /></div>
+                    <div className={classes.search_box_div}>
+                        <input className={classes.search_box} placeholder='Search for users' onKeyUp={this.userSearch} />
+                    </div>
                     <div className={classes.search_button_div}><button className={classes.search_button}>Go</button></div>
                 </div>
                 <div className={classes.search_result_container}>
                     <div className={classes.contact_list_title}>Contact List</div>
                     <div className={classes.search_result}>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
-                        <a><SearchResult><h3>Result</h3></SearchResult></a>
+                        {users}
                     </div>
                 </div>
             </SearchTemplate>
