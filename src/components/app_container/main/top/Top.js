122d0d88b7272cfcd76218617fe2ea789591537f
takunnithan <takunnithan@gmail.com>
2019-01-25 00:15:14 +0530

    wip

--- src/components/app_container/main/top/Top.js
+++ src/components/app_container/main/top/Top.js
@@ -7,10 +7,10 @@ class Top extends Component {
       <div className={classes.top} >
         <div className={classes.channel_details}>
           <div className={classes.channel_title}>#general</div>
-          <div className={classes.channel_description}> * |   20  | General channel for announcements</div>
+          <div className={classes.channel_description}>********</div>
         </div>
         <div className={classes.settings} >
-          <div className={classes.settings_list}>Settings | Settings</div>
+          <div className={classes.settings_list}>***</div>
         </div>
       </div>
     );
